81b6af405507486426ab63d33dfbfce528990d16
Jaturong Vongsunthorn <jaturong.vongsunthorn.refinitiv.com>
2022-04-01 23:07:48 +0700

    feat: init security group

--- environments/sandbox/main.tf
+++ environments/sandbox/main.tf
@@ -12,3 +12,32 @@ resource "aws_s3_bucket_acl" "sandbox_terraform_demo_acl" {
   acl    = "private"
 }
 
+resource "aws_security_group" "sandbox_web" {
+  name          = "sandbox_web"
+  description   = "Allow standard http and https ports inbound and everything outbound"
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
