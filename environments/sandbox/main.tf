fa159277fe1c375bf7e135b4735eb41ec6b261f1
Jaturong Vongsunthorn <jaturong.vongsunthorn.refinitiv.com>
2022-04-01 23:07:22 +0700

    feat: init s3 bucket

--- environments/sandbox/main.tf
+++ environments/sandbox/main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   profile = "default"
   region = "ap-southeast-1"
 }
+
+resource "aws_s3_bucket" "sandbox_terraform_demo" {
+  bucket  = "terraform-demo"
+}
+
+resource "aws_s3_bucket_acl" "sandbox_terraform_demo_acl" {
+  bucket = aws_s3_bucket.sandbox_terraform_demo.id
+  acl    = "private"
+}
+
